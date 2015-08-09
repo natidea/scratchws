c14b54cbe6c8ae2ecb5f73a8a2a7f1c31ef8b074
Nat Ayewah <nayewah@live.com>
2015-08-09 00:19:06 -0700

    Produce roslyn map

--- RoslynFileMap/RoslynFileMap/Program.cs
+++ RoslynFileMap/RoslynFileMap/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,13 +8,24 @@ using System.Threading.Tasks;
 
 namespace RoslynFileMap
 {
+    // http://www.newtonsoft.com/json/help/html/DeserializeDictionary.htm
+    // http://www.newtonsoft.com/json/help/html/SerializeDictionary.htm
     class Program
     {
         static void Main(string[] args)
         {
-            string directory = @"D:\Repos\natideavs\Analyzers";
+            string directory = args.Any() ? args.First() : @"D:\Repos\github\roslyn\src";
+
+            // get map
             var fileMap = GetFileMap(directory);
 
+            // serialize to string
+            var json = JsonConvert.SerializeObject(fileMap, Formatting.Indented);
+
+            // write to file
+            File.WriteAllText("../../output/roslyn.json", json);
+
+            Console.WriteLine($"Wrote {fileMap.Count} entries");
             Console.ReadKey();
         }
 
@@ -23,7 +35,7 @@ namespace RoslynFileMap
 
             return new DirectoryInfo(directory)
                 .GetFiles("*.cs", SearchOption.AllDirectories)
-                .Select(f => f.FullName.Substring(len + 1))
+                .Select(f => f.FullName.Substring(len + 1).Replace("\\", "/"))
                 .ToDictionary(p => p.ToLowerInvariant());
         }
     }
